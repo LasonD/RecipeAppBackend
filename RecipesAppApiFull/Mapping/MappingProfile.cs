2e99f922c4411f741a30cc7c24a18c04ace69751
Oleksandr Barylo <sbarylo20023@gmail.com>
2022-05-09 12:53:48 +0300

    Some registrations

---
+++ RecipesAppApiFull/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using RecipesAppApiFull.Dtos;
+
+namespace RecipesAppApiFull.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Ingredient, IngredientDto>();
+            CreateMap<Recipe, RecipeDto>();
+            CreateMap<User, UserDto>();
+        }
+    }
+}
--- RecipesAppApiFull/Program.cs
+++ RecipesAppApiFull/Program.cs
@@ -1,13 +1,23 @@
+using Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using RecipesAppApiFull.Mapping;
 using RecipesAppApiFull.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var config = builder.Configuration
+    .AddJsonFile("appsettings.json")
+    .Build();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(config.GetConnectionString("ApplicationDbContextConnectionString")));
 
 var app = builder.Build();
 
