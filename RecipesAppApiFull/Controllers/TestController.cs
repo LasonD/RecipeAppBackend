a6931cd97e5bf998f8bade235c53ea7f99cab346
Oleksandr Barylo <sbarylo20023@gmail.com>
2022-09-18 18:43:27 +0300

    Add test controller

---
+++ RecipesAppApiFull/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipesAppApiFull.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("")]
+        public string Test()
+        {
+            return "Hello World!";
+        }
+    }
+}
